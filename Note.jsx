d06c15db4ee21869b2af2982fd4ff8b22aa76c36
IshanNY <61487096+IshanNY@users.noreply.github.com>
2021-06-05 13:38:56 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>ShapeAI React.js</h1>
+      <p>
+        This was an amazing experience I am glad that I
+        joined to this bootcamp.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
